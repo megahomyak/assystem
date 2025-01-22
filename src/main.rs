8ffa74f3d2f612b3b72c752ef24b34baecc2c41c
megahomyak <g.megahomyak@gmail.com>
2025-01-22 07:04:32 +0500

    Wrote down more thoughts

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,9 @@ type FileIndex = u64;
 /*
 Database structure:
 all: [format name (length added to every index)] [fixed memory (like stack)] [dynamic memory (like heap)]
-fixed memory: [pair (for root)]
-pair: [0 branch index (nullable)] [1 branch index (nullable)]
-all: [format name (length added to every index)] [*first block*] [.. *rest of blocks* ..]
-first block: [whether or not there's a block after it] [root 0 branch index (nullable)] [root 1 branch index (nullable)] [value index (nullable)] <- this block has index "0", btw! And must always be present, and is unique, which is why it's fine to use an index "0" for special cases
-rest of blocks: [whether or not this block is taken] [prev block index (non-null)] [next block index (nullable)] [..any data..]
+
+Dynamic memory structure:
+block: [prev block index] [block length] [flags: whether it's a free block and whether it's a last block]
 */
 impl Fallocator {
     fn allocate(&mut self, bytes_amount: u64) -> FileIndex {
